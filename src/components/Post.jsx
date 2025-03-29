0dff4329abb8413f12ce4e09fef5c52440d3209e
Rosana Oliveira <rosanadeoliveira0215@gmail.com>
2025-03-28 22:33:59 -0300

    Comment

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,6 @@
 import styles from './Post.module.css';
- 
+import { Comment } from './Comment';
+
 export function Post() {
   return (
     <article className={styles.post}>
@@ -35,6 +36,12 @@ export function Post() {
            <button type="submit">Publicar</button>
          </footer>
       </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
 
     </article>
   )
