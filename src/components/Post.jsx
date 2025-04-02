6f8748d29564b5096604916602129ca0e0aa378e
Rosana Oliveira <rosanadeoliveira0215@gmail.com>
2025-04-02 20:40:04 -0300

    Entendendo a key

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -56,7 +56,7 @@ export function Post({ author, publishedAt, content }) {
       <div className={styles.content}>
           {content.map(line => {
            if (line.type === 'paragraph') {
-             return <p> key={line.content}</p>;
+             return <p key={line.content}>{line.content}</p>;
            } else if (line.type === 'link') {
              return <p key={line.content}><a href="#">{line.content}</a></p>
            }
